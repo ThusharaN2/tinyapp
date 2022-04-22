e68f966739a1ff197930319f1d7185f4b6e6747c
Thushara <thusharan@hotmail.com>
2022-04-21 22:05:17 -0400

    added fcn to generate random shortURL

--- express_server.js
+++ express_server.js
@@ -8,6 +8,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() { //unique shortURL
+  let randomString = "";
+  const randomChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  while (randomString.length < 6) {
+    randomString += randomChar[Math.floor(Math.random() * randomChar.length)];
+  }
+  return randomString;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,9 +37,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
