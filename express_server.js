d73d94e2ee86fffb7f513d94ea1be73867e0b414
Thushara <thusharan@hotmail.com>
2022-04-22 13:46:00 -0400

    be able to delete shortened URLS from databse

--- express_server.js
+++ express_server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 function generateRandomString() { //unique shortURL
   let randomString = "";
@@ -19,6 +18,10 @@ function generateRandomString() { //unique shortURL
   }
   return randomString;
 }
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -50,16 +53,24 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`)
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => { //removes URL resource
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
