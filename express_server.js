1b28561569e7078b34c7c36613812ec18397ca26
Thushara <thusharan@hotmail.com>
2022-04-22 21:58:41 -0400

    updated URLs

--- express_server.js
+++ express_server.js
@@ -67,6 +67,10 @@ app.post("/urls/:shortURL/delete", (req, res) => { //removes URL resource
   res.redirect("/urls");
 });
 
+app.post('/urls/:id', (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect('/urls');
+})
 
 
 app.get("/urls/:shortURL", (req, res) => {
